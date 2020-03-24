03bb37f256eaab266c09b01c2acceb526a0568e5
JosephChen <2015cjy19950905@gmail.com>
2020-03-24 23:17:30 +1100

    add firebase config

---
+++ Web/App/config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+config.py file is used for web app configuration
+"""
+
+"""
+SECRET_KEY = 'hard to guess'
+DEBUG = True
+"""
+
+firebaseConfig = {
+    "apiKey": "AIzaSyBZEUJ0unUXbOOFN9MaLcHMyGfL14hUAZk",
+    "authDomain": "aussie-celebrities-recog-72ab7.firebaseapp.com",
+    "databaseURL": "https://aussie-celebrities-recog-72ab7.firebaseio.com",
+    "projectId": "aussie-celebrities-recog-72ab7",
+    "storageBucket": "aussie-celebrities-recog-72ab7.appspot.com",
+    "messagingSenderId": "877617425337",
+    "appId": "1:877617425337:web:9d910dc5efbfb376aaa468",
+    "measurementId": "G-LY6LWF8B9Y"
+}
